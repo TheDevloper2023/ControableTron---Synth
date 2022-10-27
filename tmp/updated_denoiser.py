35263a70a0e23b7621ee76a770719a86f0632b9e
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-10-27 15:13:06 +0200

    Add new update file to fix device issues in WaveGlow repository

---
+++ tmp/updated_denoiser.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('tacotron2')
+import torch
+from layers import STFT
+from typing import Optional
+
+
+class Denoiser(torch.nn.Module):
+    """ Removes model bias from audio produced with waveglow """
+
+    def __init__(self, waveglow, filter_length=1024, n_overlap=4,
+                 win_length=1024, mode='zeros', device: Optional[str] = None):
+        super(Denoiser, self).__init__()
+        device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.stft = STFT(filter_length=filter_length,
+                         hop_length=int(filter_length/n_overlap),
+                         win_length=win_length).to(device)
+        if mode == 'zeros':
+            mel_input = torch.zeros(
+                (1, 80, 88),
+                dtype=waveglow.upsample.weight.dtype,
+                device=waveglow.upsample.weight.device)
+        elif mode == 'normal':
+            mel_input = torch.randn(
+                (1, 80, 88),
+                dtype=waveglow.upsample.weight.dtype,
+                device=waveglow.upsample.weight.device)
+        else:
+            raise Exception("Mode {} if not supported".format(mode))
+
+        with torch.no_grad():
+            bias_audio = waveglow.infer(mel_input, sigma=0.0).float()
+            bias_spec, _ = self.stft.transform(bias_audio)
+
+        self.register_buffer('bias_spec', bias_spec[:, :, 0][:, :, None])
+
+    def forward(self, audio, strength=0.1, device: Optional[str] = None):
+        device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        audio_spec, audio_angles = self.stft.transform(audio.to(device).float())
+        audio_spec_denoised = audio_spec - self.bias_spec * strength
+        audio_spec_denoised = torch.clamp(audio_spec_denoised, 0.0)
+        audio_denoised = self.stft.inverse(audio_spec_denoised, audio_angles)
+        return audio_denoised
