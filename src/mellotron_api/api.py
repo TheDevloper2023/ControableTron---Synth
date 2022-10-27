8dd37b8035474047bd2309a73100fe0432944c7c
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-10-27 15:28:19 +0200

    Fix semi-stable version

--- src/mellotron_api/api.py
+++ src/mellotron_api/api.py
@@ -7,10 +7,11 @@ from scipy.io.wavfile import write
 import librosa
 import torch
 
-from mellotron.hparams import create_hparams
-from mellotron.model import Tacotron2 as Mellotron, load_model
-from mellotron.waveglow.denoiser import Denoiser
-from mellotron.layers import TacotronSTFT
+from mellotron.hparams import create_hparams, AttrDict
+from mellotron.model import Tacotron2 as Mellotron
+from mellotron.layers import TacotronSTFT as MellotronSTFT
+from waveglow.glow import WaveGlow
+from waveglow.denoiser import Denoiser
 from mellotron.data_utils import TextMelCollate
 from mellotron.text import cmudict, text_to_sequence
 from mellotron.yin import compute_yin
@@ -21,26 +22,40 @@ from typing import Optional, Tuple
 def load_tts(
         tts_model_checkpoint_path: str,
         device: Optional[torch.device] = None,
-) -> Tuple[Mellotron, object]:
+) -> Tuple[Mellotron, MellotronSTFT, AttrDict]:
     # See:
     # https://github.com/NVIDIA/mellotron/blob/master/inference.ipynb
     device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Load hyper-parameters
+    hparams: AttrDict = create_hparams()
+    # Create STFT PyTorch layer
+    stft = MellotronSTFT(
+        hparams.filter_length,
+        hparams.hop_length,
+        hparams.win_length,
+        hparams.n_mel_channels,
+        hparams.sampling_rate,
+        hparams.mel_fmin,
+        hparams.mel_fmax
+    )
+    # Create Mellotron TTS instance and load weights
+    mellotron: Mellotron = Mellotron(hparams).to(device).eval()
+    mellotron.load_state_dict(torch.load(tts_model_checkpoint_path, map_location=device)['state_dict'])
 
-    hparams = create_hparams()
-    mellotron = load_model(hparams).to(device).eval()
-    mellotron.load_state_dict(torch.load(tts_model_checkpoint_path)['state_dict'])
-
-    return mellotron, hparams
+    return mellotron, stft, hparams
 
 
 def load_vocoder(
         vocoder_model_checkpoint_path: str,
         device: Optional[torch.device] = None
-) -> Tuple[torch.nn.Module, Denoiser]:
+) -> Tuple[WaveGlow, Denoiser]:
     # See:
     # https://github.com/NVIDIA/mellotron/blob/master/inference.ipynb
-    waveglow = torch.load(vocoder_model_checkpoint_path)['model'].to(device).eval()
-    denoiser = Denoiser(waveglow).cuda().eval()
+    device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Load vocoder
+    waveglow: WaveGlow = torch.load(vocoder_model_checkpoint_path, map_location=device)['model'].eval()
+    # Load denoiser
+    denoiser: Denoiser = Denoiser(waveglow).to(device).eval()
 
     return waveglow, denoiser
 
@@ -54,8 +69,10 @@ def load_arpabet_dict(dict_path: str):
 
 
 def _get_mel_spec(
-        audio_path: str,
-        hparams,
+        stft: MellotronSTFT,
+        hparams: AttrDict,
+        audio_path: Optional[str] = None,
+        audio: Optional[np.ndarray] = None,
         device: Optional[torch.device] = None
 ) -> torch.FloatTensor:
     # See:
@@ -63,22 +80,16 @@ def _get_mel_spec(
     # https://github.com/NVIDIA/mellotron/blob/master/inference.ipynb
     device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Create STFT PyTorch layer
-    stft = TacotronSTFT(
-        hparams.filter_length,
-        hparams.hop_length,
-        hparams.win_length,
-        hparams.n_mel_channels,
-        hparams.sampling_rate,
-        hparams.mel_fmin,
-        hparams.mel_fmax
-    )
+    # Enforce either audio path or audio data but not both
+    assert (audio_path is not None and audio is None) or (audio_path is None and audio is not None)
+
     # Load audio file
-    audio, sampling_rate = librosa.core.load(audio_path, sr=hparams.sampling_rate)
+    if audio_path is not None:
+        audio, _ = librosa.core.load(audio_path, sr=hparams.sampling_rate)
     # Convert to PyTorch format
     audio = torch.from_numpy(audio)
-    # Normalize
-    audio_norm = audio / hparams.max_wav_value
+    # Normalize (If not already in [-1; 1] range)
+    audio_norm = audio / hparams.max_wav_value if audio.max() > 1.0 or audio.min() < -1.0 else audio
     audio_norm = audio_norm.unsqueeze(0)
     # Compute Mel spectrogram using utility layer
     mel_spec = stft.mel_spectrogram(audio_norm)
@@ -88,8 +99,10 @@ def _get_mel_spec(
 
 
 def _get_f0(
-        hparams,
+        hparams: AttrDict,
         audio_path: Optional[str] = None,
+        audio: Optional[np.ndarray] = None,
+        stft: Optional[MellotronSTFT] = None,
         mel_spec: Optional[np.ndarray] = None,
         device: Optional[torch.device] = None
 ) -> torch.FloatTensor:
@@ -97,15 +110,20 @@ def _get_f0(
     # https://github.com/NVIDIA/mellotron/blob/master/data_utils.py
     device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Enforce either audio path or Mel spectrogram but not both
-    assert not (audio_path is None and mel_spec is None) and not (audio_path is not None and mel_spec is not None)
+    # Enforce either audio path or audio, but not both, and either STFT or Mel spectrogram, but not both
+    assert (audio_path is not None and audio is None) or (audio_path is None and audio is not None)
+    assert (stft is not None and mel_spec is None) or (stft is None and mel_spec is not None)
+
+    # Load audio file
+    if audio_path is not None:
+        audio, _ = librosa.core.load(audio_path, sr=hparams.sampling_rate)
 
     # Get input Mel Spectrogram
-    mel_spec = mel_spec if mel_spec is not None else _get_mel_spec(audio_path, hparams, device=device)
+    mel_spec = mel_spec if mel_spec is not None else _get_mel_spec(stft, hparams, audio, device=device).cpu().numpy()
 
     # Compute F0
     f0, harmonic_rates, argmins, times = compute_yin(
-        mel_spec,
+        audio,
         hparams.sampling_rate,
         hparams.filter_length,
         hparams.hop_length,
@@ -119,7 +137,7 @@ def _get_f0(
     # Convert to PyTorch  format
     f0 = np.array(f0, dtype=np.float32)
     f0 = torch.from_numpy(f0)[None]
-    f0 = f0[:, :mel_spec.shape[1]]
+    f0 = f0[:, :mel_spec.shape[-1]]
 
     return f0
 
@@ -128,13 +146,14 @@ def _get_f0(
 def get_prosody_embedding(
         reference_audio_path: str,
         mellotron: Mellotron,
-        hparams,
+        stft: MellotronSTFT,
+        hparams: AttrDict,
         device: Optional[torch.device] = None
 ) ->torch.FloatTensor:
     device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Load Mel spectrogram
-    mel_spec = _get_mel_spec(reference_audio_path, hparams, device=device)
+    mel_spec = _get_mel_spec(reference_audio_path, stft, hparams, device=device)
     # Compute prosody embedding via reference encoder
     prosody_embedding = mellotron.gst.encoder(mel_spec)
 
@@ -145,13 +164,14 @@ def get_prosody_embedding(
 def get_gst(
         reference_audio_path: str,
         mellotron: Mellotron,
-        hparams,
+        stft: MellotronSTFT,
+        hparams: AttrDict,
         device: Optional[torch.device] = None
 ) -> torch.FloatTensor:
     device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Load Mel spectrogram
-    mel_spec = _get_mel_spec(reference_audio_path, hparams, device=device)
+    mel_spec = _get_mel_spec(reference_audio_path, stft, hparams, device=device)
 
     ...
 
@@ -161,7 +181,8 @@ def encode_input(
         reference_audio_path: str,
         arpabet_dict,
         mellotron: Mellotron,
-        hparams,
+        stft: MellotronSTFT,
+        hparams: AttrDict,
         speaker_id: Optional[int] = None,
         device: Optional[torch.device] = None
 ) -> Tuple[torch.LongTensor, torch.FloatTensor, torch.LongTensor, torch.FloatTensor]:
@@ -172,10 +193,13 @@ def encode_input(
 
     # Encode text
     text = torch.LongTensor(text_to_sequence(text, hparams.text_cleaners, arpabet_dict), device=device)[None, :]
+
+    # Encode audio
+    audio, _ = librosa.core.load(reference_audio_path, sr=hparams.sampling_rate)
     # Compute Reference audio Mel spectrogram
-    mel_spec = _get_mel_spec(reference_audio_path, hparams, device=device)
+    mel_spec = _get_mel_spec(stft, hparams, audio=audio, device=device)
     # Compute pitch contour
-    f0 = _get_f0(hparams, mel_spec=mel_spec.cpu().numpy(), device=device)
+    f0 = _get_f0(hparams, audio=audio, mel_spec=mel_spec.squeeze().cpu().numpy(), device=device)
     # Encode speaker ID
     # If speaker ID is none sample one randomly from model
     if speaker_id is None:
@@ -214,8 +238,9 @@ def synthesise_speech(
         text: str,
         reference_audio_path: str,
         mellotron: Mellotron,
-        hparams,
-        waveglow,
+        stft: MellotronSTFT,
+        hparams: AttrDict,
+        waveglow: WaveGlow,
         denoiser: Denoiser,
         arpabet_dict,
         speaker_id: Optional[int] = None,
@@ -231,7 +256,7 @@ def synthesise_speech(
 
     # Encode inputs
     text, style_input, speaker_ids, f0s = encode_input(
-        text, reference_audio_path, arpabet_dict, mellotron, hparams, speaker_id=speaker_id, device=device
+        text, reference_audio_path, arpabet_dict, mellotron, stft, hparams, speaker_id=speaker_id, device=device
     )
     encoded_input = (text, gst_style if gst_style is not None else style_input, speaker_ids, f0s)
 
